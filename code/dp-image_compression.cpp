05e1fe32223d25fcc9630b3ac5312e61f7c514e4
minghong <100890258+minghong@users.noreply.github.com>
2024-03-17 17:26:48 +0800

    Update dp-image_compression.cpp

--- code/dp-image_compression.cpp
+++ code/dp-image_compression.cpp
@@ -1,4 +1,4 @@
-// 图像压缩
+//dp-compress
 
 #include<bits/stdc++.h>
 
@@ -8,7 +8,7 @@ int const n=256*256;
 int p[n+1];
 int s[n+1], l[n+1], b[n+1];
 
-int length(int i)  //计算像素点p所需要的存储位数
+int length(int i)  
 {
     int k = 1;
     i = i/2;
@@ -19,25 +19,24 @@ int length(int i)  //计算像素点p所需要的存储位数
     }
     return k;
 }
-void Compress(int n, int p[], int s[], int l[], int b[])  //令s[i]为前i个段最优合并的存储位数
+void Compress(int n, int p[], int s[], int l[], int b[])  
 {
     int Lmax = 256, header = 11;
     s[0] = 0;
-    for(int i=1; i<=n; i++)  //i表示前几段
+    for(int i=1; i<=n; i++)  
     {
-        b[i] = length(p[i]); //计算像素点p需要的存储位数
+        b[i] = length(p[i]); 
         int bmax = b[i];
-        s[i] = s[i-1] + bmax;  //故下面j从2开始
+        s[i] = s[i-1] + bmax;  
         l[i] = 1;
-        for(int j=2; j<=i && j<=Lmax; j++)   //递推关系:s[i]=  min(1<=j<=i)(lsum(i-j+1, i)<=256) {s[i-j]+ lsum(i-j+1,i)*bmax(i-j+1,i) } + 11
+        for(int j=2; j<=i && j<=Lmax; j++)   
         {
             if(bmax < b[i-j+1])
                 bmax = b[i-j+1];
-            if(s[i] > s[i-j] + j*bmax)   //因为一开始所有序列并没有分段,所以可以看作每一段就是一个数,故lsum(i-j+1, i) = j;
+            if(s[i] > s[i-j] + j*bmax)   
             {
                 s[i] = s[i-j] + j*bmax;
-                l[i] = j;   //最优断开位置,l[i]表示前i段的最优分段方案中应该是在i-j处断开  比如l[5] = 3,这表示前五段的最优分段应该是(5-3=2)处断开,s[5] = s[2] + 3*bmax
-                            //即 12 | 345,以此类推,得到l[n];之后构造最优解时再由l[n]向前回溯
+                l[i] = j;   
             }
         }
         s[i] += header;
@@ -47,7 +46,7 @@ void Traceback(int n, int &m, int s[], int l[])
 {
     if(n == 0) return;
     Traceback(n-l[n], m, s, l);
-    s[m++] = n-l[n];  //重新为s[]数组赋值，用来存储分段位置
+    s[m++] = n-l[n];  
 }
 void Output(int s[], int l[], int b[], int n)
 {
@@ -75,11 +74,11 @@ void test()
 	cout << "-----------test----------- " << endl;
 	ifstream in("D:\\1.txt");
 	string line;
-	while (getline(in, line)){//获取文件的一行字符串到line中
-		stringstream ss(line);//初始化 法1
+	while (getline(in, line)){
+		stringstream ss(line);
 		int x;
 		int i=1;
-		while (ss >> x){//每一行包含不同个数的数字
+		while (ss >> x){
 
 			p[i++]=int(x);
 		}
@@ -103,6 +102,4 @@ int main()
     return 0;
 }
 
-/*6
-10 12 15 255 1 2*/
 
