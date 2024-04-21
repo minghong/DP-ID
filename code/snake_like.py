b2d6532a2beefc46a1b2eb177b0d1d4c8ac79aff
minghong <100890258+minghong@users.noreply.github.com>
2024-04-21 09:32:58 +0800

    Add files via upload

---
+++ code/snake_like.py
@@ -0,0 +1,29 @@
+import numpy as np
+from PIL import Image
+import re
+import numpy as np
+
+import random
+
+
+
+def image_to_bitstream(image_path):
+
+
+    img = Image.open(image_path)
+    img_arr = np.array(img)
+    
+    
+    return img_arr
+
+bitstream=image_to_bitstream("D:\\lena1.bmp")
+out=open("D:\\1.txt","w")
+
+for i in range(len(bitstream)):
+    if(i%2==0):
+        for j in range(len(bitstream[0])):
+            out.write(str(bitstream[i][j])+" ")
+    else:
+        for j in range(len(bitstream[0])-1,-1,-1):
+            out.write(str(bitstream[i][j])+" ")
+out.close()
