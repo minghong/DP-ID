f31db421cd2baad620d0473c023f38ccf360d5b4
minghong <100890258+minghong@users.noreply.github.com>
2024-04-21 09:43:33 +0800

    Update correct_length.py

--- code/correct_length.py
+++ code/correct_length.py
@@ -1,56 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 21 11:58:00 2024
-
-@author: 11353
-"""
-import random
-import numpy as np
-
-file=open("new.txt")
-new=open('mutation_dna.txt',"w")
-for line in file.readlines():
-    if(line[0]!=">"):
-        new.write(mutate(line.split("\n")[0])+"\n")
-
-new.close()
-
-out=open("new.txt","w")
-file=open("mutation_dna.txt")
-for l in file.readlines():
-    l=''.join(l.split())
-    l=l.rstrip("\n")
-    length=len(l)
-    if(length==150):
-        out.write(l+"\n")
-    elif(length>150):
-        while(length>150):
-            l = l[:75] + l[76:]
-            length-=1
-        out.write(l+"\n")
-    else:
-        while(length<150):
-            l = l[:75] +"C" +l[75:]
-            length+=1
-        out.write(l+"\n")
-
-out.close()
-
-
-'''
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 21 11:58:00 2024
-
-@author: 11353
-"""
 import random
 import numpy as np
 
 
 
 out=open("D:\\process.txt","w")
-file=open("D:\\mutation.txt")
+file=open("D:\\mutation_dna.txt")
 for l in file.readlines():
     l=''.join(l.split())
     l=l.rstrip("\n")
@@ -69,4 +23,3 @@ for l in file.readlines():
         out.write(l+"\n")
 
 out.close()
-'''
