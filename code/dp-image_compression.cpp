2fc9ccd4384a5698437a6be8e71f67953b9fee7e
minghong <100890258+minghong@users.noreply.github.com>
2024-04-21 10:07:01 +0800

    Update dp-image_compression.cpp

--- code/dp-image_compression.cpp
+++ code/dp-image_compression.cpp
@@ -1,74 +1,15 @@
-//dp-compress
-
+#include<iostream>
+#include<cmath>
+#include<stack>
 #include<bits/stdc++.h>
-
 using namespace std;
 
-int const n=256*256;
-int p[n+1];
-int s[n+1], l[n+1], b[n+1];
-
-int length(int i)  
-{
-    int k = 1;
-    i = i/2;
-    while(i>0)
-    {
-        k++;
-        i = i/2;
-    }
-    return k;
-}
-void Compress(int n, int p[], int s[], int l[], int b[])  
-{
-    int Lmax = 256, header = 11;
-    s[0] = 0;
-    for(int i=1; i<=n; i++)  
-    {
-        b[i] = length(p[i]); 
-        int bmax = b[i];
-        s[i] = s[i-1] + bmax;  
-        l[i] = 1;
-        for(int j=2; j<=i && j<=Lmax; j++)   
-        {
-            if(bmax < b[i-j+1])
-                bmax = b[i-j+1];
-            if(s[i] > s[i-j] + j*bmax)   
-            {
-                s[i] = s[i-j] + j*bmax;
-                l[i] = j;   
-            }
-        }
-        s[i] += header;
-    }
-}
-void Traceback(int n, int &m, int s[], int l[])
-{
-    if(n == 0) return;
-    Traceback(n-l[n], m, s, l);
-    s[m++] = n-l[n];  
-}
-void Output(int s[], int l[], int b[], int n)
-{
-    cout<<"The optimal value is "<<s[n]<<endl;
-    int m = 0;
-    Traceback(n, m, s, l);
-    s[m] = n;
-    cout<<"Decompose into "<<m<<" segments "<<endl;
-    for(int j=1; j<=m; j++)
-    {
-        l[j] = l[s[j]];
-        b[j] = b[s[j]];
-    }
-    ofstream outfile("D:\\result2.txt");
-    for(int j=1; j<=m; j++)
-
-        outfile << l[j] << " "<<b[j]<<endl;
-    outfile.close();
-
-}
-
-
+const int N = 256*256+1;
+int p[N+1];
+int length(int i);
+void Compress(int n,int p[],int s[],int l[],int b[]);
+int TraceBack(int n,int l[],int b[]);  
+void Out(int m,int min_len,int l[],int b[]);
 void test()
 {
 	cout << "-----------test----------- " << endl;
@@ -81,25 +22,100 @@ void test()
 		while (ss >> x){
 
 			p[i++]=int(x);
+
 		}
 
 	}
 }
 
-
 int main()
 {
 
-
     test();
-    Compress(n, p, s, l, b);
-    int m=0;
-    Traceback(n, m, s, l);
-    Output(s, l, b, n);
-    memset(p, sizeof(p), 0);
+    int s[N]={0},l[N]={0},b[N]={0};
 
 
+    Compress(N-1,p,s,l,b);
+    int m=TraceBack(N-1,l,b);
+    Out(m,s[N-1],l,b);
     return 0;
 }
 
+void Compress(int n,int p[],int s[],int l[],int b[])
+{
+    int Lmax = 256,header = 11;
+    s[0] = 0;
+    for(int i=1; i<=n; i++)
+    {
+        b[i] = length(p[i]);
+        int bmax = b[i];
+        s[i] = s[i-1] + bmax + header;
+        l[i] = 1;
 
+        for(int j=2; j<=i && j<=Lmax;j++)  
+        {
+
+            if(bmax<length(p[i-j+1]))
+            {
+                bmax = length(p[i-j+1]);
+            }
+
+            if(s[i]>s[i-j]+j*bmax+header)
+            {
+                s[i] = s[i-j] + j*bmax+header;
+                l[i] = j;
+                b[i] = bmax; 
+            }
+        }
+    }
+}
+
+int length(int i)
+{
+    int k=1;
+    i = i/2;
+    while(i>0)
+    {
+        k++;
+        i=i/2;
+    }
+    return k;
+
+}
+
+int TraceBack(int n,int l[],int b[]) 
+{
+    stack<int>ss;
+    ss.push(l[n]);
+    ss.push(b[n]);
+    while (n!=0)
+    {
+        n=n-l[n];
+        ss.push(l[n]);  
+        ss.push(b[n]);
+    }
+    int i=0;
+    while (!ss.empty())
+    {
+        b[i]=ss.top();
+        ss.pop();
+        l[i]=ss.top(); 
+        ss.pop();
+        i++;
+    }
+    return i-1;
+}
+
+void Out(int m,int min_len,int l[],int b[])
+{
+    int i=0;
+    cout<<"最小长度："<<min_len<<endl;
+    cout<<"共分成："<<m<<"段"<<endl;
+    ofstream outfile("D:\\result2.txt");
+    for(int j=1; j<=m; j++)
+    {
+        outfile << l[j] << " "<<b[j]<<endl;
+    }
+
+    outfile.close();
+}
